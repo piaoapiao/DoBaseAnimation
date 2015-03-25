76e795dc5b7211926287ef40e5cac15a65f2b717
piaoapiao <guodong@guodongdeMacBook-Pro.local>
2015-03-25 16:47:40 +0800

    Initial Commit

---
+++ DoBaseAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoBaseAnimation
+//
+//  Created by guodong on 15/3/25.
+//  Copyright (c) 2015年 guodong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoBaseAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoBaseAnimation
+//
+//  Created by guodong on 15/3/25.
+//  Copyright (c) 2015年 guodong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
